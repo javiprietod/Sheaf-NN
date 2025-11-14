fd5d891e6fe92685b9253d91a13339d16f4fe807
Sergio Herreros <202113370@alu.comillas.edu>
2025-11-14 11:43:10 +0100

    Notebook with basic GCN for ogbg-moltox21.

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,20 @@
-from ogb.graphproppred import PygGraphPropPredDataset
+import warnings
 
-dataset = PygGraphPropPredDataset(name='ogbg-molhiv')
+warnings.filterwarnings("ignore")
+
+from ogb.graphproppred import PygGraphPropPredDataset
+from torch_geometric.loader import DataLoader
+
+dataset = PygGraphPropPredDataset(name="ogbg-moltox21")
 print(dataset)
+
+split_idx = dataset.get_idx_split()
+train_loader = DataLoader(dataset[split_idx["train"]], batch_size=32, shuffle=True)
+valid_loader = DataLoader(dataset[split_idx["valid"]], batch_size=32, shuffle=False)
+test_loader = DataLoader(dataset[split_idx["test"]], batch_size=32, shuffle=False)
+
+# Load first batch
+batch = next(iter(train_loader))
+print(batch)
+print("Node features:")
+print(batch.x)  # Node features
